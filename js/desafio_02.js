6f8e340ab88a3de7f68ce4b6cbe9954f6d291571
KAUAN PINHEIRO DO NASCIMENTO <kauan.pnascimento@senacsp.edu.br>
2023-09-27 11:16:46 -0300

    desafio 02

---
+++ js/desafio_02.js
@@ -0,0 +1,25 @@
+
+
+
+function botao(){
+
+    let v1,v2,v3;
+
+    v1 = document.querySelector("#N1").value;
+    v2 = document.querySelector("#N2").value;
+
+    console.log(`o valor add é ${v1}`);
+    console.log(`o valor add é ${v2}`);
+
+    v3 = v1 * v2
+
+    console.log(`test ${v3}`)           
+
+    document.getElementById("Re").innerHTML = v3
+
+}
+
+
+
+
+
